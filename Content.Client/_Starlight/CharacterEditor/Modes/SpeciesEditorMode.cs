546228f3a13c5699b56732935654973c749c2da6
Jezithyr <Jezithyr@gmail.com>
2025-11-16 17:35:26 -0800

    wip

--- Content.Client/_Starlight/CharacterEditor/Modes/SpeciesEditorMode.cs
+++ Content.Client/_Starlight/CharacterEditor/Modes/SpeciesEditorMode.cs
@@ -1,7 +1,7 @@
 ﻿// SPDX-FileCopyrightText: 2025 Starlight Network
 // SPDX-License-Identifier: Starlight-MIT
 
-using Content.Client._Starlight.CharacterEditor.Editors.BodyType;
+using Content.Client._Starlight.CharacterEditor.Editors;
 using Content.Client._Starlight.CharacterEditor.Editors.Species;
 
 namespace Content.Client._Starlight.CharacterEditor.Modes;
