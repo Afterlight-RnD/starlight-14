9a7d3d31a99889dea461ef8f119eff121a130aee
Jezithyr <Jezithyr@gmail.com>
2026-04-11 16:44:46 -0700

    documentation pass 1

--- Content.Shared/_Starlight/Body/Events/BodyEvents.cs
+++ Content.Shared/_Starlight/Body/Events/BodyEvents.cs
@@ -4,10 +4,42 @@
 using Content.Shared._Starlight.Body.Components;
 namespace Content.Shared._Starlight.Body.Events;
 
+public delegate void BodyPartEventHandler<in TEvent>(
+    Entity<SLBodyComponent> body,
+    Entity<SLBodyPartComponent> bodyPart,
+    TEvent args) where TEvent: notnull;
+public delegate void BodyPartRefEventHandler<TEvent>(
+    Entity<SLBodyComponent> body,
+    Entity<SLBodyPartComponent> bodyPart,
+    ref TEvent args) where TEvent: notnull;
 
+public delegate void ConditionalBodyPartEventHandler<TEvent>(
+    Entity<SLBodyComponent> body,
+    Entity<SLBodyPartComponent> bodyPart,
+    TEvent args,
+    BodyPartEventCondition<TEvent> conditional)
+    where TEvent: notnull;
+public delegate void ConditionalBodyPartRefEventHandler<TEvent>(
+    Entity<SLBodyComponent> body,
+    Entity<SLBodyPartComponent> bodyPart,
+    ref TEvent args,
+    BodyPartRefEventCondition<TEvent> conditional)
+    where TEvent: notnull;
 
-public delegate void BodyPartEventHandler<in TEvent>(Entity<SLBodyComponent> body, Entity<SLBodyPartComponent> bodyPart, TEvent args) where TEvent: notnull;
-public delegate void BodyPartRefEventHandler<TEvent>(Entity<SLBodyComponent> body, Entity<SLBodyPartComponent> bodyPart, ref TEvent args) where TEvent: notnull;
+public delegate bool BodyPartEventCondition<in TEvent>(
+    IDependencyCollection deps,
+    IEntityManager entityManager,
+    Entity<SLBodyComponent> body,
+    Entity<SLBodyPartComponent> bodyPart,
+    TEvent args) where TEvent : notnull;
+
+public delegate bool BodyPartRefEventCondition<TEvent>(
+    IDependencyCollection deps,
+    IEntityManager entityManager,
+    Entity<SLBodyComponent> body,
+    Entity<SLBodyPartComponent> bodyPart,
+    ref TEvent args) where TEvent : notnull;
+
 
 public record struct PartAddedToBodyEvent(Entity<SLBodyPartComponent> Part);
 
--- Content.Shared/_Starlight/Body/Systems/BodySystem.Events.cs
+++ Content.Shared/_Starlight/Body/Systems/BodySystem.Events.cs
@@ -9,64 +9,176 @@ namespace Content.Shared._Starlight.Body.Systems;
 
 public sealed partial class BodySystem
 {
+    /// <summary>
+    /// Subscribes to an entity event on a body *from* a BodyPart. Can only be called when Event subscriptions are unlocked such as from within EntitySystem.Initialize!
+    /// </summary>
+    /// <param name="subscriber">subscription owner</param>
+    /// <param name="handler">event handler</param>
+    /// <typeparam name="TEvent">event type</typeparam>
+    /// <returns>returns true if successful, false if it already exists</returns>
     public bool SubscribeBodyEvent<TEvent>(IEntityEventSubscriber subscriber, BodyPartEventHandler<TEvent> handler)
-        where TEvent: notnull => EnsureEventSubs<TEvent>().Subscribe(subscriber, handler);
+        where TEvent : notnull => EnsureEventSubs<TEvent>().Subscribe(subscriber, handler);
 
+    /// <summary>
+    /// Subscribes to an entity event on a body *from* a BodyPart. Can only be called when Event subscriptions are unlocked such as from within EntitySystem.Initialize!
+    /// </summary>
+    /// <param name="subscriber">subscription owner</param>
+    /// <param name="handler">event handler</param>
+    /// <typeparam name="TEvent">event type</typeparam>
+    /// <returns>returns true if successful, false if it already exists</returns>
     public bool SubscribeBodyEvent<TEvent>(IEntityEventSubscriber subscriber, BodyPartRefEventHandler<TEvent> handler)
-        where TEvent: notnull => EnsureEventSubs<TEvent>().Subscribe(subscriber, handler);
+        where TEvent : notnull => EnsureEventSubs<TEvent>().Subscribe(subscriber, handler);
 
-    public bool UnSubscribeBodyEvent<TEvent>(IEntityEventSubscriber subscriber)
+    /// <summary>
+    /// Unsubscribe a BodyPart from a body entity event. (Optionally) remove the relays if they are empty
+    /// WARNING: relay events can only be added *once* per game lifetime and clearing them will remove them for the rest of the game lifecycle!
+    /// </summary>
+    /// <param name="subscriber"></param>
+    /// <param name="unsubRelays"></param>
+    /// <typeparam name="TEvent"></typeparam>
+    /// <returns></returns>
+    public bool UnSubscribeBodyEvent<TEvent>(IEntityEventSubscriber subscriber, bool unsubRelays = false)
         where TEvent : notnull
     {
         var success = false;
-        if (!TryGetEventSubs<TEvent>(out var subs))
+        if (!TryGetEventRegistry<TEvent>(out var subs))
             return success;
-        if (subs.RegisteredListeners.Remove(subscriber, out var removed))
-        {
-            subs.Handlers -= removed;
-            success = true;
-        }
-        if (!subs.RegisteredRefListeners.Remove(subscriber, out var removedRef)) return success;
-        subs.RefHandlers -= removedRef;
-        success = true;
+        subs.ClearListener(subscriber, unsubRelays);
         return success;
     }
-    private Dictionary<Type, ISubs> _bodyEventRelays = new();
 
-    private bool TryGetEventSubs<TEvent>([NotNullWhen(true)]out Subs<TEvent>? subs) where TEvent : notnull
+    #region Event Relay Implementation
+
+    /// <summary>
+    /// Tries to get event registry by type
+    /// </summary>
+    /// <param name="register">found register</param>
+    /// <typeparam name="TEvent">event type</typeparam>
+    /// <returns>true if successful</returns>
+    private bool TryGetEventRegistry<TEvent>([NotNullWhen(true)] out SubRegisterRegister<TEvent>? register) where TEvent : notnull
     {
         var subType = typeof(TEvent);
-        subs = null;
-        if (!_bodyEventRelays.TryGetValue(subType, out var rawSubs)) return false;
-        subs = (Subs<TEvent>)rawSubs;
+        register = null;
+        if (!_eventRelayRegistry.TryGetValue(subType, out var rawSubs)) return false;
+        register = (SubRegisterRegister<TEvent>)rawSubs;
         return true;
     }
 
-    private Subs<TEvent> EnsureEventSubs<TEvent>() where TEvent : notnull
+    /// <summary>
+    /// Attempts to remove an event register. (Optionally) will clear relays as well.
+    /// WARNING: relay events can only be added *once* per game lifetime and clearing them will remove them for the rest of the game lifecycle!
+    /// </summary>
+    /// <param name="clearRelays">should we clear relays? Warning: only can do once per game lifecycle!</param>
+    /// <typeparam name="TEvent">event type</typeparam>
+    /// <returns></returns>
+    private bool RemoveEventRegister<TEvent>(bool clearRelays = false) where TEvent : notnull
     {
         var subType = typeof(TEvent);
-        if (!_bodyEventRelays.TryGetValue(subType, out var rawSubs))
+        if (!_eventRelayRegistry.TryGetValue(subType, out var registry))
+            return false;
+        registry.ClearListeners(clearRelays);
+        return true;
+    }
+
+    /// <summary>
+    /// Ensure that a specific Event has a registry. Can only be called when Event subscriptions are unlocked such as from within EntitySystem.Initialize!
+    /// </summary>
+    /// <typeparam name="TEvent">event type</typeparam>
+    /// <returns>created event sub</returns>
+    private SubRegisterRegister<TEvent> EnsureEventSubs<TEvent>() where TEvent : notnull
+    {
+        var subType = typeof(TEvent);
+        if (!_eventRelayRegistry.TryGetValue(subType, out var rawSubs))
         {
-            var subs = new Subs<TEvent>(EntityManager);
-            _bodyEventRelays.Add(subType, subs);
+            var subs = new SubRegisterRegister<TEvent>(EntityManager);
+            _eventRelayRegistry.Add(subType, subs);
             return subs;
         }
-       return (Subs<TEvent>)rawSubs;
+
+        return (SubRegisterRegister<TEvent>)rawSubs;
     }
 
+    /// <summary>
+    /// Registry of all body event subscription registers indexed by event type.
+    /// Lazily populated during system initialization when body events are subscribed to
+    /// </summary>
+    private Dictionary<Type, ISubRegister> _eventRelayRegistry = new();
 
-    private interface ISubs: IEntityEventSubscriber
+    /// <summary>
+    /// A register of body event subscriptions, stores handler signatures and performs dispatch to listeners
+    /// </summary>
+    private interface ISubRegister : IEntityEventSubscriber
     {
+        /// <summary>
+        /// EventBus Accessor
+        /// </summary>
         IEventBus EventBus { get; }
+
+        /// <summary>
+        /// EntityManager dependency
+        /// </summary>
         IEntityManager EntMan { get; }
 
-        void UnsubscribeRelays();
+        /// <summary>
+        /// Clears all relay events!
+        /// WARNING: relay events can only be added *once* per game lifetime and clearing them will remove them for the rest of the game lifecycle!
+        /// </summary>
+        void ClearRelays();
+
+        /// <summary>
+        /// Clears an event subscriber's listeners
+        /// WARNING: registries can only be added *once* per game lifetime and clearing them will remove them for the rest of the game lifecycle!
+        /// </summary>
+        /// <param name="clearRegistryIfEmpty">should we clear event registries as well?</param>
+        void ClearListener(IEntityEventSubscriber subscriber, bool clearRegistryIfEmpty = false);
+
+        /// <summary>
+        /// Clears all listeners and (optionally) relay events.
+        /// WARNING: relay events can only be added *once* per game lifetime and clearing them will remove them for the rest of the game lifecycle!
+        /// </summary>
+        /// <param name="clearRelays">should we clear relay events as well?</param>
+        void ClearListeners(bool clearRelays = false);
+
+        /// <summary>
+        /// Clear all data, use for cleanup ONLY
+        /// </summary>
+        void ClearAll();
+
+        /// <summary>
+        /// Do we have any listeners registered?
+        /// </summary>
+        bool HasListeners { get; }
     }
 
-    private sealed class Subs<TEvent> : ISubs
+    /// <summary>
+    /// Typed register, these are created at initialization and share lifecycle with bodySystem.
+    /// These are created *once* lazily when BodyEvents are subscribed/registered.
+    /// </summary>
+    /// <typeparam name="TEvent">The event being relayed</typeparam>
+    private sealed class SubRegisterRegister<TEvent> : ISubRegister
         where TEvent : notnull
     {
-        public Subs(IEntityManager entMan)
+        public IEventBus EventBus { get; }
+        public IEntityManager EntMan { get; }
+        public event BodyPartEventHandler<TEvent>? Handlers;
+        public event BodyPartRefEventHandler<TEvent>? RefHandlers;
+
+        public event BodyPartEventCondition<TEvent>? ConditionalHandlers;
+        public event BodyPartRefEventCondition<TEvent>? ConditionalRefHandlers;
+        public Dictionary<IEntityEventSubscriber, BodyPartEventHandler<TEvent>> RegisteredListeners { get; } = new();
+
+        public Dictionary<IEntityEventSubscriber, BodyPartRefEventHandler<TEvent>> RegisteredRefListeners { get; } =
+            new();
+
+        public Dictionary<IEntityEventSubscriber, BodyPartEventCondition<TEvent>> RegisteredConditionalListeners { get; } = new();
+
+        public Dictionary<IEntityEventSubscriber, BodyPartRefEventCondition<TEvent>> RegisteredConditionalRefListeners { get; } =
+            new();
+
+
+        public bool HasListeners => Handlers != null && RefHandlers != null && ConditionalHandlers != null && ConditionalRefHandlers != null;
+
+        public SubRegisterRegister(IEntityManager entMan)
         {
             EventBus = entMan.EventBus;
             EntMan = entMan;
@@ -91,9 +203,36 @@ public sealed partial class BodySystem
                 RelayToBodyEvents(body, bodyPart, ref args);
             }
         }
-        public void UnsubscribeRelays()
+
+        public void ClearRelays()
         {
-            EventBus.UnsubscribeLocalEvent<SLBodyComponent,TEvent>();
+            EventBus.UnsubscribeLocalEvent<SLBodyComponent, TEvent>();
+        }
+
+        public void ClearListener(IEntityEventSubscriber subscriber, bool clearRegistryIfEmpty = false)
+        {
+            if (RegisteredListeners.Remove(subscriber, out var handler))
+                Handlers -= handler;
+            if (RegisteredRefListeners.Remove(subscriber, out var refHandler))
+                RefHandlers -= refHandler;
+            if (clearRegistryIfEmpty && !HasListeners)
+                ClearRelays();
+        }
+
+        public void ClearListeners(bool clearRelays = false)
+        {
+            RegisteredListeners.Clear();
+            RegisteredRefListeners.Clear();
+            Handlers = null;
+            RefHandlers = null;
+
+            if (clearRelays)
+                ClearRelays();
+        }
+
+        public void ClearAll()
+        {
+            ClearListeners(true);
         }
 
         public bool Subscribe(IEntityEventSubscriber subscriber,
@@ -116,30 +255,13 @@ public sealed partial class BodySystem
             return true;
         }
 
-        public IEventBus EventBus { get; }
-        public IEntityManager EntMan { get; }
-        public event BodyPartEventHandler<TEvent>? Handlers;
-        public event BodyPartRefEventHandler<TEvent>? RefHandlers;
-        public Dictionary<IEntityEventSubscriber, BodyPartEventHandler<TEvent>> RegisteredListeners { get; } = new();
-        public Dictionary<IEntityEventSubscriber,BodyPartRefEventHandler<TEvent>> RegisteredRefListeners { get; } = new();
-
         public void RelayToBodyEvents(Entity<SLBodyComponent> body, Entity<SLBodyPartComponent> bodyPart, TEvent args)
             => Handlers?.Invoke(body, bodyPart, args);
 
-        public void RelayToBodyEvents(Entity<SLBodyComponent> body, Entity<SLBodyPartComponent> bodyPart, ref TEvent args)
+        public void RelayToBodyEvents(Entity<SLBodyComponent> body, Entity<SLBodyPartComponent> bodyPart,
+            ref TEvent args)
             => RefHandlers?.Invoke(body, bodyPart, ref args);
     }
 
-    private void CleanUpListeners()
-    {
-
-    }
-
-    private void CleanUpRelaySubscriptions()
-    {
-        foreach (var (_, relay) in _bodyEventRelays)
-        {
-          relay.UnsubscribeRelays();
-        }
-    }
+    #endregion
 }
